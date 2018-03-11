443b5fd21a25e766f31fbfd0c892cfaaa33d838e
MuhammedFasilK <mhdfasilwyd@gmail.com>
2018-03-11 11:49:51 +0530

    Add files via upload

---
+++ Smart_Street_Light.ino
@@ -0,0 +1,27 @@
+int ldrpin=A0;
+int dstpin=12;
+int ledpin=10;
+int light=0;
+int dist=0;
+void setup() {
+pinMode(A0,INPUT);
+pinMode(12,INPUT);
+pinMode(10,OUTPUT);
+}
+
+void loop() {
+  light=analogRead(ldrpin);
+  dist=digitalRead(dstpin);
+  if(light<700)
+  { 
+    analogWrite(ledpin,128);
+    if(dist==HIGH)
+    {
+      analogWrite(ledpin,255);
+    }
+   else
+   {
+    analogWrite(ledpin,0);
+    }
+  }
+}
