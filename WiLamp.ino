53b46ec3859fcb993770ed46f7a3da685f31f59e
Muhammed Fasil K <mhdfasilwyd@gmail.com>
2020-03-03 15:31:58 +0530

    Update WiLamp.ino

--- WiLamp.ino
+++ WiLamp.ino
@@ -1,16 +1,20 @@
 // WiLamp Version 1
-// Made by Muhammed Fasil K-https://github.com/mhdfasilwyd/Arduino_Projects
+// Made by Muhammed Fasil K - https://github.com/mhdfasilwyd/Arduino_Projects
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#define LED 2 // LED Pin
+const char* deviceName = "Wilamp_V1"; 
+const char* ssid = "wilamp";
+const char* password = "12345678";
 const char MAIN_page[] PROGMEM = R"=====(
   <!DOCTYPE html>
   <html>
   <body>
   <center>
-  <h1>WiLamp</h1><br>
-  Ciclk to turn <a href="ledOn">LED ON</a><br>
-  Ciclk to turn <a href="ledOff">LED OFF</a><br>
+  <h1>WiLamp V1</h1><br>
+  Click to turn <a href="ledOn">LED ON</a><br>
+  Click to turn <a href="ledOff">LED OFF</a><br>
   </center> 
   </body>
   </html>
@@ -18,11 +22,7 @@ const char MAIN_page[] PROGMEM = R"=====(
 IPAddress staticIP(192, 168, 43, 90); 
 IPAddress gateway(192, 168, 43, 1);   
 IPAddress subnet(255, 255, 255, 0);  
-IPAddress dns(8, 8, 8, 8);  //DNS
-const char* deviceName = "Wilamp_V1";
-#define LED 2  
-const char* ssid = "wilamp";
-const char* password = "12345678";
+IPAddress dns(8, 8, 8, 8);
 ESP8266WebServer server(80); 
 void handleRoot() 
 {
@@ -59,7 +59,7 @@ void setup(void){
   server.on("/ledOn", handleLEDon); 
   server.on("/ledOff", handleLEDoff);
   server.begin();                 
-  Serial.println("WiLamp Server Started");
+  Serial.println("WiLamp V1 Server Started");
 }
 void loop(void){
 server.handleClient();         
