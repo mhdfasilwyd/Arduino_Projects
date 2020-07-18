c1a96ab660734b1135551e87dc035793da3eef13
Muhammed Fasil K <mhdfasilwyd@gmail.com>
2020-07-18 13:10:15 +0530

    Update Smart_Street_Light.ino

--- Smart_Street_Light.ino
+++ Smart_Street_Light.ino
@@ -1,15 +1,17 @@
-int ldrpin=A0;
-int dstpin=12;
-int ledpin=10;
+#define ldrpin A0
+#define dstpin 12
+#define ledpin 10
 int light=0;
 int dist=0;
-void setup() {
-pinMode(A0,INPUT);
-pinMode(12,INPUT);
-pinMode(10,OUTPUT);
+void setup() 
+{
+  pinMode(A0,INPUT);
+  pinMode(12,INPUT);
+  pinMode(10,OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   light=analogRead(ldrpin);
   dist=digitalRead(dstpin);
   if(light<700)
