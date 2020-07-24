403e3f82a9710bbde86ee2a1197e6215e9bf4f68
Muhammed Fasil K <mhdfasilwyd@gmail.com>
2020-07-24 17:44:13 +0530

    Update IR-PWM_LED_Control.ino

--- IR-PWM_LED_Control.ino
+++ IR-PWM_LED_Control.ino
@@ -1,36 +1,41 @@
 #include <IRremote.h> 
-int RECV_PIN =3; 
-int led1 = 5;
-int ledpwm = 255; 
-#define code1  33444015 
-#define code2  33478695 
+
+#define RECV_PIN 3 
+#define LED_PIN 5
+#define LED_PWM 255
+#define CODE_A  33444015 
+#define CODE_B  33478695 
+
 IRrecv irrecv(RECV_PIN);
 decode_results results;
+
 void setup()
 {
-irrecv.enableIRIn();  
-pinMode(led1, OUTPUT);
-analogWrite(led1,ledpwm); 
+  irrecv.enableIRIn();  
+  pinMode(LED_PIN, OUTPUT);
+  analogWrite(LED_PIN,LED_PWM); 
 } 
-void loop() {
-if (irrecv.decode(&results)) {
-unsigned int value = results.value;
-switch(value) {
-case code1:
-{ 
-ledpwm = ledpwm + 5;      
-analogWrite(led1,ledpwm);             
-} 
-break; 
-case code2:
+
+void loop() 
 {
-ledpwm = ledpwm - 5;     
-analogWrite(led1,ledpwm);
-} 
-break;           
+  if (irrecv.decode(&results)) 
+  {
+    unsigned int value = results.value;
+    switch(value) 
+    {
+      case CODE_A:
+      { 
+        LED_PWM = LED_PWM + 5;      
+        analogWrite(LED_PIN,LED_PWM);             
+      } 
+    break; 
+      case CODE_B:
+      {
+        LED_PWM = LED_PWM - 5;     
+        analogWrite(LED_PIN,LED_PWM);
+      } 
+    break;           
+    }
+  irrecv.resume(); 
+  }
 }
-irrecv.resume(); 
-}
-}
-
-
